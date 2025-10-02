b4f7973eadcd2d76e010fbc8e5291ef5d8dc0c65
kaushalyap <24698778+kaushalyap@users.noreply.github.com>
2025-10-02 11:07:59 +0530

    Removed util.go and moved CreateFile to defer.go since solutions directory defer.go fail to detect CreateFile function since  it is in different folders. Also added TODO comment to clarify where student code should go.

--- internal/exercises/solutions/28_defer/defer.go
+++ internal/exercises/solutions/28_defer/defer.go
@@ -3,10 +3,28 @@ package deferr
 import (
 	"fmt"
 	"os"
+	"sync"
+)
+
+var (
+	fileInstance *os.File
+	once         sync.Once
 )
 
 func WriteToFile(*os.File) {
 	f := CreateFile()
 	defer f.Close()
 	fmt.Fprintln(f, "data")
+}
+
+// Create a singleton of a temporary file
+func CreateFile() *os.File {
+	once.Do(func() {
+		f, err := os.CreateTemp("", "tmpfile")
+		if err != nil {
+			panic(err)
+		}
+		fileInstance = f
+	})
+	return fileInstance
 }
--- internal/exercises/templates/28_defer/defer.go
+++ internal/exercises/templates/28_defer/defer.go
@@ -3,9 +3,29 @@ package deferr
 import (
 	"fmt"
 	"os"
+	"sync"
+)
+
+var (
+	fileInstance *os.File
+	once         sync.Once
 )
 
 func WriteToFile(*os.File) {
 	f := CreateFile()
+	// TODO: your code here
+
 	fmt.Fprintln(f, "ABC")
+}
+
+// Create a singleton of a temporary file
+func CreateFile() *os.File {
+	once.Do(func() {
+		f, err := os.CreateTemp("", "tmpfile")
+		if err != nil {
+			panic(err)
+		}
+		fileInstance = f
+	})
+	return fileInstance
 }
